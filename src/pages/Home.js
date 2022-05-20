f25baae934c3bd8a4056a5e94df61a0cf20c1791
seokjeongheon <jingg1112@naver.com>
2022-05-20 22:16:08 +0900

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import img1 from '../imgg/11.jpg'
+import img2 from '../imgg/12.jpg'
 import Button from '@mui/material/Button';
+import styled from 'styled-components'
+
 
 function Home() {
   return (
-      <Button variant="contained">시작화면</Button>
-  )
+    <Zzzzzz>
+    <Img1>
+      <img src={img1} alt="imgg/11.jpg"></img>
+    </Img1>
+    <Img2>
+     <img src={img2} alt="imgg/12.jpg"></img>
+    </Img2>
+    </Zzzzzz>
+
+  );
 }
 
 export default Home
+
+const Img1 = styled.div`
+  max-width: 100%;
+  float: left;
+  background-color:black
+`
+const Img2 = styled.div`
+  max-width: 50%;
+  float: right;
+  background-color:blue
+  
+`
+const Zzzzzz = styled.div`
+  background-color:red
+`
