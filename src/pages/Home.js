81ac849aa78ad0cfe58c5674e6e07660776584f2
seokjeongheon <jingg1112@naver.com>
2022-05-23 19:25:49 +0900

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,35 +3,32 @@ import img1 from '../imgg/11.jpg'
 import img2 from '../imgg/12.jpg'
 import Button from '@mui/material/Button';
 import styled from 'styled-components'
+import { positions, width } from '@mui/system';
+// import "./home.css";
 
 
 function Home() {
   return (
     <Zzzzzz>
-    <Img1>
-      <img src={img1} alt="imgg/11.jpg"></img>
-    </Img1>
-    <Img2>
-     <img src={img2} alt="imgg/12.jpg"></img>
-    </Img2>
+      <div style={{positions:'reletive'}}>
+      <button >시작</button>
+      <img src={img1} alt="imgg/11.jpg" width='900' height='965' ></img>
+      <img src={img2} alt="imgg/12.jpg" width='960' height='965' ></img>
+      </div>
     </Zzzzzz>
-
+    
   );
 }
 
 export default Home
 
 const Img1 = styled.div`
-  max-width: 100%;
-  float: left;
-  background-color:black
+ 
 `
 const Img2 = styled.div`
-  max-width: 50%;
-  float: right;
-  background-color:blue
+
   
 `
 const Zzzzzz = styled.div`
-  background-color:red
+  background-color:black;
 `
