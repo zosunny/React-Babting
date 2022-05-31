e578ac3b54310c02b17b2d86d1167d177e99a646
seokjeongheon <jingg1112@naver.com>
2022-05-31 20:39:48 +0900

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import bgimg from '../imgg/salad.jpg'
+import Button from 'react-bootstrap/Button';
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom';
+
+export const Home = () => {
+  const navigate = useNavigate()
+  const handleClickButton = () => {
+    navigate('/main')
+  }
+  return (
+    <Container>
+      <Header>
+        <BGImg>
+          <img src={bgimg} alt="홈이미지" />
+        </BGImg>
+        <Title>밥오밥나무가 추천하는 오늘의 메뉴를 알고싶으신가요?zzddzz</Title>
+        <Button variant="outline-warning" style={{}} onClick={handleClickButton}>추천받기</Button>
+      </Header>
+    </Container>
+    
+  );
+}
+
+export default Home
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
+const Header = styled.div`
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+`
+const Title = styled.div`
+  font-size: 20pt;
+`
+const BGImg = styled.div`
+  
+  margin: 0 0 0 0;
+`
---
+++ src/pages/Main.js
@@ -0,0 +1,72 @@
+import React,{ useState, useEffect} from 'react'
+import {ProgressBar,Button} from 'react-bootstrap';
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom';
+import "./Main.css";
+
+const items = [
+  {
+    name:'파스타',
+    src:require('../imgg/pasta.jpg'),
+    dec:'매운거땡긴다',
+  },
+  {
+    name:'초밥',
+    src:require('../imgg/21.jpg'),
+    dec:'초밥떙김ddddd'
+  },
+  {
+    name:'매운맛',
+    src:require('../imgg/매운사진.png'),
+    dec:'asdfazzzzzsf'
+  },
+  {
+    name:'달달한',
+    src:require('../imgg/달콤.webp'),
+    dec:'hhhhhhhhhhhh'
+  },
+]
+
+
+const Main = () => {
+  const [foods, setFoods] = useState([]);
+  const [displays, setDisplays] = useState([]);
+  const [winners, setWinners] = useState([]);
+  const [questno,setquestno] = useState(0);
+  console.log('questno',questno)
+
+
+  useEffect(() => {
+    setFoods(items);
+    setDisplays([items[0], items[1]]);
+  }, []);
+
+  const clickHandler = (food) => (d) => {
+    onclick={}
+  };
+  return (
+    <Ddd>
+       <ProgressBar striped variant="danger" now={100} 
+        style= {{ marginTop: '20px'}}/>
+      <h1 className='title'>바오밥나무</h1>
+      {displays.map((d) => {
+        return (
+          <div className='flex-1' key={d.name} onClick={clickHandler(d)}>
+            <img className='food-img' src={d.src} />
+            <div className='food-name'>{d.name}</div>
+            <div className='food-dec'>{d.dec}</div>
+          </div>
+        );
+      })}
+    </Ddd>
+  );
+};
+
+
+export default Main
+
+const Ddd = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
+`
