373f001efcde3bdab956450e54bf9c199c309e65
zomochii <zoheyy108@naver.com>
2022-06-03 20:32:55 +0900

    home modify commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,25 +25,26 @@ export const Home = () => {
 export default Home
 
 const Container = styled.div`
-  font-family: 'ROKABold';
+  //font-family: 'StrongArmy';
   width: 100%;
   height: calc(100% - 56px);
-  // position: relative;
-  //z-index: 1;
+  position: relative;
+  z-index: 1;
   background:url('../imgg/salad.jpg');
   background-size:cover;
-  /* &::ater {
+   &::after {
     content: "";
     width: 100%;
-    height: calc(100% - 56px);
-    background:url('../imgg/salad.jpg');
-    background-size:cover;
+    height: 100%;
+    background-color: #000;
     position: absolute;
     top: 0;
     left: 0;
+    right: 0;
+    bottom: 0;
     z-index: -1;
     opacity: 0.5;
-  } */
+  }
 `
 const Contents = styled.div`
   height:100%;
