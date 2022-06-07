d01ba1882c89b648b4978830eb63cc75b5602133
zomochii <zoheyy108@naver.com>
2022-06-08 01:48:13 +0900

    resultD commit

---
+++ src/data/foodResultD.js
@@ -0,0 +1,114 @@
+export const ResultData = [
+  {
+    id: 1,
+    food: "짬뽕 ㄱㄱ",
+    combi: "A1B1C1D1",
+    desc: "맵고 따듯 국물있고 만원이하",
+    image: `${'../imgg/짬뽕.jpg'}`
+  },
+  {
+    id: 2,
+    food: "매운탕",
+    combi: "A1B1C1D2",
+    desc: "맵고 따듯 국물있고 만원이상",
+    image: `${'../imgg/매운탕.jpg'}`
+  },
+  {
+    id: 3,
+    food: "떡볶이",
+    combi: "A1B1C2D1",
+    desc: "맵고 따듯 국물없고 만원이하",
+    image: `${'../imgg/엽떡매운맛.jpg'}`
+  },
+  {
+    id: 4,
+    food: "김치말이국수",
+    combi: "A1B2C1D1",
+    desc: "맵고 차갑 국물있고 만원이하",
+    image:`${'../imgg/김치말이.jpg'}`
+  },
+  {
+    id: 5,
+    food: "아구찜",
+    combi: "A1B1C2D2",
+    desc: "맵고 따듯 국물없고 만원이상",
+    image: `${'../imgg/아구찜.jpg'}`
+  },
+  {
+    id: 6,
+    food: "취두부",
+    combi: "A1B2C1D2",
+    desc: "맵고 차갑 국물있고 만원이상",
+    image: `${'../imgg/취두부.jpg'}`
+  },
+  {
+    id: 7,
+    food: "비빔면",
+    combi: "A1B2C2D1",
+    desc: "맵고 차갑 국물없고 만원이하",
+    image:`${'../imgg/비빔면.jpg'}`
+  },
+  {
+    id: 8,
+    food: "해파리냉채",
+    combi: "A1B2C2D2",
+    desc: "맵고 차갑 국물없고 만원이상",
+    image: `${'../imgg/해파리.jpg'}`
+  },
+  {
+    id: 9,
+    food: "설렁탕",
+    combi: "A2B1C1D1",
+    desc: "안맵고 따듯 국물있고 만원이하",
+    image: `${'../imgg/설렁탕.jpg'}`
+  },
+  {
+    id: 10,
+    food: "물냉면",
+    combi: "A2B2C1D1",
+    desc: "안맵고 차갑 국물있고 만원이하",
+    image: `${'../imgg/물냉.jpg'}`
+  },
+  {
+    id: 11,
+    food: "호박고구마",
+    combi: "A2B1C2D1",
+    desc: "안맵고 따듯 국물없고 만원이하",
+    image: `${'../imgg/호박고구마.jpg'}`
+  },
+  {
+    id: 12,
+    food: "연포탕",
+    combi: "A2B1C1D2",
+    desc: "안맵고 따듯 국물있고 만원이상",
+    image: `${'../imgg/연포탕.jpg'}`
+  },
+  {
+    id: 13,
+    food: "아이스크림",
+    combi: "A2B2C2D1",
+    desc: "안맵고 차갑 국물없고 만원이하",
+    image: `${'../imgg/아이스크림.jpg'}`
+  },
+  {
+    id: 14,
+    food: "족발",
+    combi: "A2B1C2D2",
+    desc: "안맵고 따듯 국물없고 만원이상",
+    image: `${'../imgg/족발1.jpg'}`
+  },
+  {
+    id: 15,
+    food: "냉모밀정식",
+    combi: "A2B2C1D2",
+    desc: "안맵고 차갑 국물있고 만원이상",
+    image: `${'../imgg/냉모밀정식.jpg'}`
+  },
+  {
+    id: 16,
+    food: "육회",
+    combi: "A2B2C2D2",
+    desc: "안맵고 차갑 국물없고 만원이상",
+    image: `${'../imgg/육회.jpg'}`
+  },
+]
